b57234160e576682ef5fa8288c4ef80ca4ba6ac2
Christian Giraudo <christian.giraudo@estudiantxs.uacm.edu.mx>
2023-11-16 22:39:42 -0600

    Datos tres dígitos

--- datos.cpp
+++ datos.cpp
@@ -3,8 +3,10 @@
 
 // Arreglo:
 const int tamagno = 50;
-const int arreglo[tamagno] = {784971, 753851, 846339, 884863, 927879, 124281, 530940, 507438, 733001, 780295, 446947, 849799, 510923, 344456, 943752, 140416, 518999, 980061, 447068, 820839, 537615, 827404, 510294, 680390, 683586, 353285, 212648, 183831, 148683, 544654, 775492, 920523, 777853, 542923, 478914, 679950, 891279, 963965, 147806, 274735, 471676, 417906, 666572, 965782, 524035, 110810, 519099, 267003, 543478, 640144};
+const int arreglo[tamagno] = {134, 213, 767, 801, 396, 733, 820, 726, 755, 880, 660, 374, 559, 333, 325, 784, 631, 662, 844, 718, 127, 892, 984, 976, 282, 498, 253, 516, 504, 612, 890, 296, 691, 733, 880, 917, 365, 743, 847, 432, 470, 977, 369, 202, 620, 198, 442, 131, 495, 463};
 /* 'const' indica que la variable no puede ser modificada después 
  * de haber sido inicializada.
  * Los datos del arreglo se generaron en LibreOffice Calc, con la función
- * '=ALEATORIO.ENTRE(100000,999999)'. */
+ * '=ALEATORIO.ENTRE(100,999)'.
+ * Nota: Cuando se usaron datos de seis dígitos mediante la función
+ * '=ALEATORIO.ENTRE(100000,999999)', la media no mostró decimales. */
